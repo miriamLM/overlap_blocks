5007293edf145dcfd20c18cbb937f61b233939a2
Miriam Lopez Martinez <miriam.mlm95@gmail.com>
2022-06-13 20:33:52 +0200

    create service for explode the input blocks (no functionality yet)

---
+++ src/Overlap/ApplicationService/InputBlockService.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OverlapBlocks\Overlap\ApplicationService;
+
+final class InputBlockService
+{
+    public function __invoke(ExplodeInputBlocksRequest $explodeInputBlocksRequest)
+    {
+    }
+}
