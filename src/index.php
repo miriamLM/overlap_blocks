852ab9a292e0c515bb98bbe06dbe2a8460c470b2
Miriam Lopez Martinez <miriam.mlm95@gmail.com>
2022-06-13 19:48:35 +0200

    the InputBlocksCommandController class is instantiated and the explodeInputBlocks function is called

--- src/index.php
+++ src/index.php
@@ -2,15 +2,16 @@
 
 declare(strict_types=1);
 
+use OverlapBlocks\Overlap\Infrastructure\InputBlocksCommandController;
+
 require_once '../vendor/autoload.php';
 
 if (isset($argv)) {
-
     try {
-
-        $blockinput = count($argv) === 1 ? "" : $argv[1];
-        echo "$blockinput";
-
+        $blockInput = count($argv) === 1 ? "" : $argv[1];
+        //echo "$blockInput";
+        $inputBlockCommandController = new InputBlocksCommandController();
+        $inputBlockCommandController->explodeInputBlocks($blockInput);
     } catch (RuntimeException $exception) {
     }
 
