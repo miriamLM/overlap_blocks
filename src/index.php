b3959b0fc015dbc6ded78220dcec26b6d0fc8175
Miriam Lopez Martinez <miriam.mlm95@gmail.com>
2022-06-14 19:25:49 +0200

    instantiate CreateBlockBoardService and pass it as attribute to the controller of the instance of CreateBlockBoardController. Pass the width and length (hardcoded as 5 and 6)

--- src/index.php
+++ src/index.php
@@ -4,16 +4,24 @@ declare(strict_types=1);
 
 use OverlapBlocks\Overlap\ApplicationService\InputBlockService;
 use OverlapBlocks\Overlap\Infrastructure\InputBlocksCommandController;
+use OverlapBlocks\Shared\ApplicationService\CreateBlockBoardService;
+use OverlapBlocks\Shared\Infrastructure\CreateBlockBoardController;
+
 
 require_once '../vendor/autoload.php';
+
+$createBlockBoardService = new CreateBlockBoardService();
+$createBlockBoardController = new CreateBlockBoardController($createBlockBoardService);
+$createBlockBoardController(5,6);
 
 if (isset($argv)) {
     try {
         $blockInput = count($argv) === 1 ? "" : $argv[1];
-        //echo "$blockInput";
+
         $inputBlockService = new InputBlockService();
         $inputBlockCommandController = new InputBlocksCommandController($inputBlockService);
         $inputBlockCommandController->explodeInputBlocks($blockInput);
+
     } catch (RuntimeException $exception) {
     }
 
