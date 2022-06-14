5f68f63b13c200c7a2fd9b8fff60451ccd6c97cd
Miriam Lopez Martinez <miriam.mlm95@gmail.com>
2022-06-14 19:54:09 +0200

    create a new board

--- src/Shared/ApplicationService/CreateBlockBoardService.php
+++ src/Shared/ApplicationService/CreateBlockBoardService.php
@@ -5,7 +5,12 @@ declare(strict_types=1);
 namespace OverlapBlocks\Shared\ApplicationService;
 
 
+use OverlapBlocks\Shared\Domain\BlockBoard;
+
 final class CreateBlockBoardService
 {
-
+    public function createBlockBoardRequestWithSize(SizeBlockBoardRequest $sizeBlockBoardRequest): void
+    {
+        $blockBoard = new BlockBoard($sizeBlockBoardRequest->widthValue(),$sizeBlockBoardRequest->legthValue());
+    }
 }
