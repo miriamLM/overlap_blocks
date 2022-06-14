83cbf0d017d191df346a941dd6ff7115fd1c2262
Miriam Lopez Martinez <miriam.mlm95@gmail.com>
2022-06-14 18:57:30 +0200

    create DTO to transport data of the input blocks

---
+++ src/Overlap/ApplicationService/ExplodeInputBlocksRequest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OverlapBlocks\Overlap\ApplicationService;
+
+
+final class ExplodeInputBlocksRequest
+{
+    private string $inputBlocks;
+
+    public function __construct(string $inputBlocks)
+    {
+        $this->inputBlocks = $inputBlocks;
+    }
+
+    public function value(): string
+    {
+        return $this->inputBlocks;
+    }
+}
