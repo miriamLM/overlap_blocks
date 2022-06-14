5373563099fcbaa8643b1eb8fe75221542cc91c3
Miriam Lopez Martinez <miriam.mlm95@gmail.com>
2022-06-14 18:56:09 +0200

    update InputBlockService with explodeInputBlocks function and its implementation

--- src/Overlap/ApplicationService/InputBlockService.php
+++ src/Overlap/ApplicationService/InputBlockService.php
@@ -6,7 +6,45 @@ namespace OverlapBlocks\Overlap\ApplicationService;
 
 final class InputBlockService
 {
-    public function __invoke(ExplodeInputBlocksRequest $explodeInputBlocksRequest)
+
+    public function explodeInputBlocks(ExplodeInputBlocksRequest $explodeInputBlocksRequest)
     {
+        $firstBlock = "";
+        $secondBlock = "";
+
+        $firstCharOrientation = false;
+        $secondCharOrientation = false;
+
+        for ($i = 0; $i < strlen($explodeInputBlocksRequest->value()); $i++) {
+            if (false === $secondCharOrientation) {
+                $firstBlock = $firstBlock . $explodeInputBlocksRequest->value()[$i];
+            } else {
+                $secondBlock = $secondBlock . $explodeInputBlocksRequest->value()[$i];
+            }
+
+            if ($this->isChar(
+                    $explodeInputBlocksRequest->value()[$i]
+                ) && true === $firstCharOrientation && false === $secondCharOrientation) {
+
+                $secondCharOrientation = true;
+
+                $secondBlock = $secondBlock . $explodeInputBlocksRequest->value()[$i - 1];
+                $secondBlock = $secondBlock . $explodeInputBlocksRequest->value()[$i];
+
+                $firstBlock = substr($firstBlock, 0, -2);
+            }
+
+            if ($this->isChar($explodeInputBlocksRequest->value()[$i]) && false === $firstCharOrientation) {
+                $firstCharOrientation = true;
+            }
+        }
+    }
+
+    private function isChar(string $inputChar): bool
+    {
+        if (preg_match('/^[a-z]+$/i', $inputChar)) {
+            return true;
+        }
+        return false;
     }
 }
